5f35be46d48ee5c79aff7170e1bd2b445a189804
khatrisrx <86415582+khatrisrx@users.noreply.github.com>
2021-06-25 12:25:26 +0530

    Add files via upload

---
+++ SeleniumAssignment.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="D:\\chromedriver_win32\\chromedriver.exe")
+driver.get("https://xornet.xoriant.com")
+
+#print(driver.current_url)
+#print(driver.title)
+#driver.maximize_window()
+#driver.close()
+#print("Browser is closed")
+
+driver.find_element_by_id("userNameInput").send_keys("xor-ind\khatri_s")
+driver.find_element_by_name("Password").send_keys("") # Enter users password
+driver.find_element_by_id("submitButton").click()
+print("Done")
+driver.get("https://xornet.xoriant.com/xoriant-holidays")
+
+#driver.find_element_by_css_selector("input[type='email']").send_keys("xor-ind\khatri_s")
+#driver.find_element_by_css_selector("input[type='password']").send_keys("Delta@21")
+
